c5d5413c6733e657e442c0cda6611e536ec74005
RAFIRAF <rafalplaszczyk.com@gmail.com>
2017-04-10 18:09:11 +0200

    s

--- src/exceptions/LoggingExceptions2.java
+++ src/exceptions/LoggingExceptions2.java
@@ -1,7 +1,17 @@
 package exceptions;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
 /**
  * Created by Rafal on 02017-04-10.
  */
 public class LoggingExceptions2 {
+    private static Logger logger = Logger.getLogger("LoggingExceptions2");
+    static void logException(Exception e){
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
 }
