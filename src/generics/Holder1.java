350987496b500a7fa27f396a6911a8b8c67a37f1
RAFIRAF <rafalplaszczyk.com@gmail.com>
2017-04-26 09:33:50 +0200

    Added generics/Holder1

---
+++ src/generics/Holder1.java
@@ -0,0 +1,11 @@
+package generics;
+
+/**
+ * Created by Rafal on 02017-04-26.
+ */
+class Automobile{}
+public class Holder1 {
+    private Automobile a;
+    public Holder1(Automobile a){this.a=a;}
+    Automobile get(){return a;}
+}
