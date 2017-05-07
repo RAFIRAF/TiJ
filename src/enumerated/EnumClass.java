ba0cc0cb84242bff636ecf691d6efd5b580064e0
RAFIRAF <rafalplaszczyk.com@gmail.com>
2017-05-07 13:43:26 +0200

    Understanding enumerated/EnumClass.java

---
+++ src/enumerated/EnumClass.java
@@ -0,0 +1,15 @@
+package enumerated;
+
+/**
+ * Created by Rafal on 02017-05-07.
+ */
+enum Shrubbery {GROUND, CRAWLING, HANGING}
+
+public class EnumClass {
+    public static void main(String[] args) {
+        for (Shrubbery s : Shrubbery.values()
+             ) {
+            
+        }
+    }
+}
